250c50a20b622e0b5736c151a186a86fc55f5a37
Zaltu <swwouf@hotmail.com>
2021-05-23 13:59:31 -0400

    archive multi struct

--- src/archiver.py
+++ src/archiver.py
@@ -90,20 +90,14 @@ def _movefile(source, destination):
     shutil.move(source, destination)
 
 
-def processarchive(archive):
+def _validatedestination(config):
     """
-    Archive the folder to the correct location, and update the DB with the information on this dataset.
+    Validate if the target directory exists, and whether this file has already been archived.
 
-    :param str archive: path to the archive top dir
+    :param dict config: this archive's config
 
-    :raises SkipError: in a lot of places
+    :raises SkipError: if the target directory doesn't exist, or the data has already been archived.
     """
-    # Read config
-    config = readconfig(archive)
-
-    # Create the archive-type object, which will validate the config.
-    archiveobj = makearchive(config)
-
     # If the directory where we intend to put the archive doesn't exist, it should be created manually.
     if not os.path.exists(os.path.dirname(config["path"])):
         raise SkipError("ERROR: Destination path does not exist\n%s" % os.path.dirname(config["path"]))
@@ -111,6 +105,46 @@ def processarchive(archive):
     # If the full destination path already exists, this dataset has already been archived.
     if os.path.exists(config["path"]):
         raise SkipError("ERROR: Archive appears to already exist:\n%s" % config["path"])
+
+
+def _finalizeandupload(compressedlocation, config, archiveobj):
+    """
+    Validate compressed file, move to expected location, and update DB.
+
+    :param str compressedlocation: expected location to the compressed file to archive
+    :param dict config: this archive's config
+    :param Archive archiveobj: validated archive object for DB update
+
+    :raises SkipError: if compressed file cannot be found, or cannot be moved
+    """
+    # If the compressed file does not exist, we have a problem...
+    if not os.path.exists(compressedlocation):
+        raise SkipError("ERROR: Compression has not generated the expected file. Could not find:\n%s" % compressedlocation)
+
+    # Move compressed files to location
+    try:
+        _movefile(compressedlocation, config["path"])
+    except:
+        raise SkipError("ERROR: Unable to move compressed file... Manual intervention required.")
+
+    # Update DB
+    dbupdater.insertarchive(archiveobj)
+
+
+def processonearchive(archive, config):
+    """
+    Archive the folder to the correct location, and update the DB with the information on this dataset.
+
+    :param str archive: path to the archive top dir
+    :param dict config: the loaded archive config
+
+    :raises SkipError: in a lot of places
+    """
+    # Create the archive-type object, which will validate the config.
+    archiveobj = makearchive(config)
+
+    # Validate check for destination path and existing archive
+    _validatedestination(config)
 
     compressedlocation = detectcompressed(archive)
     if not compressedlocation:
@@ -124,15 +158,53 @@ def processarchive(archive):
         _compress(archive, archivename)
         compressedlocation = os.path.join(os.path.dirname(archive), os.path.basename(config["path"]))
 
-    # If the compressed file does not exist, we have a problem...
-    if not os.path.exists(compressedlocation):
-        raise SkipError("ERROR: Compression has not generated the expected file. Skipping.")
+    # Validate compressed file, move to archive location, and update DB
+    _finalizeandupload(compressedlocation, config, archiveobj)
 
-    # Move compressed files to location
-    try:
-        _movefile(compressedlocation, config["path"])
-    except:
-        raise SkipError("ERROR: Unable to move compressed file... Manual intervention required.")
 
-    # Update DB
-    dbupdater.insertarchive(archiveobj)
+def processarchiveset(archivepath, config):
+    """
+    Process an archive that was defined in a multi archive set. Multiple aspects of the validation logic are
+    different in this situation, so the whole thing is separated.
+
+    :param str archivepath: path to the compressed file or single directory of this archive.
+    :param dict config: the config for this particular archive
+    """
+    # Create the archive-type object, which will validate the config.
+    archiveobj = makearchive(config)
+
+    # Validate check for destination path and existing archive
+    _validatedestination(config)
+
+    if os.path.isdir(archivepath):
+        # Get the name of the archive file to store
+        try:
+            archivename = os.path.basename(config["path"]).split(".")[0]
+        except:
+            raise SkipError("ERROR: Cannot determine intended archive name from config:\n%s" % config["path"])
+        # Compress folder
+        _compress(archivepath, archivename)
+        compressedlocation = os.path.join(os.path.dirname(archivepath), os.path.basename(config["path"]))
+    elif archivepath.split(".")[-1] in ARCHIVE_FILETYPES:
+        compressedlocation = archivepath
+    else:
+        raise SkipError("Multi-Archive Sets expect pre-compressed files, or folders. Received:\n%s" % archivepath)
+
+    # Validate compressed file, move to archive location, and update DB
+    _finalizeandupload(compressedlocation, config, archiveobj)
+
+
+def processarchive(directory):
+    """
+    Process a folder, which may have multiple archives prepared in it.
+
+    :param str directory: path to the directory which contains the archive(s)
+    """
+    # Read config
+    config = readconfig(directory)
+
+    if "type" in config and config["type"] in TYPEMAP:
+        processonearchive(directory, config)
+    else:
+        for archive in config:
+            processarchiveset(archive, config[archive])
